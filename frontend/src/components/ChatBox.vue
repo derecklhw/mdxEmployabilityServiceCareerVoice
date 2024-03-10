39556598294b2908f8292c9b625abd7d19ce1770
dereck lhw <dereck.lamhonwah@gmail.com>
2024-03-10 11:05:02 +0400

    feat: add chat container ui and logic

--- frontend/src/components/ChatBox.vue
+++ frontend/src/components/ChatBox.vue
@@ -1,4 +1,5 @@
 <template lang="">
+    <div class="flex gap-1">
     <div class="flex-1 relative border-2 border-gray-300 p-3 rounded-lg flex">
         <input
             v-model="message"
@@ -10,6 +11,7 @@
             <button class="border-gray-300 border px-2 py-1 rounded">On Off</button>
             <button @click="sendChats">Send</button>
         </div>
+    </div>
     </div>
 </template>
 <script setup lang="ts">
@@ -26,13 +28,18 @@ async function sendChats() {
         content: message.value
     })
     try {
-        const response = await axios.post('http://localhost:3000/openai', {
-            message: CHATS.value
-        });
+        // const response = await axios.post('http://localhost:3000/openai', {
+        //     message: CHATS.value
+        // });
+
+        // let assistantMessage: CHAT = {
+        //     role: response.data.role,
+        //     content: response.data.content
+        // }
 
         let assistantMessage: CHAT = {
-            role: response.data.role,
-            content: response.data.content
+            role: "assistant",
+            content: "Hello, I am the MDX Employability Service Career Voice. How can I help you today?"
         }
 
         CHATS.value.push(assistantMessage)
---
+++ frontend/src/components/ChatsContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div ref="chatContainer">
+        <Message v-for="(chat, index) of CHATS" :key="index" :content="chat.content" :role="chat.role" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import Message from './Message.vue';
+import { CHATS } from '../stores/chat';
+
+const chatContainer = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+    chatContainer.value?.scrollTo(0, chatContainer.value.scrollHeight);
+});
+
+</script>
