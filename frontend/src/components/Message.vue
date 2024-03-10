41c6b792597d79cca3e6168c0f6d8e5af33171ba
dereck lhw <dereck.lamhonwah@gmail.com>
2024-03-10 11:05:17 +0400

    feat: chat message logic and ui

---
+++ frontend/src/components/Message.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex gap-1">
+      <div>
+        <div v-if="isUser" class="font-bold">You</div>
+        <div v-else class="font-bold">MDX Employability Service Career Voice</div>
+        <div>{{ content }}</div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { computed } from "vue";
+  
+  const props = defineProps<{
+    content: string;
+    role: string;
+  }>();
+  
+  const isUser = computed(() => {
+    return props.role === "user";
+  });
+  </script>
