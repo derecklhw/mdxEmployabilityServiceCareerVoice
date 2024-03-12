a879b27c53baef77fd01ae017ccb064ebf7d9ed7
dereck lhw <dereck.lamhonwah@gmail.com>
2024-03-12 09:17:18 +0400

    feat: add icons for sound and speaker

--- frontend/src/components/SoundButton.vue
+++ frontend/src/components/SoundButton.vue
@@ -1,11 +1,16 @@
 <template>
-     <div class="absolute bottom-8 left-8 cursor-pointer"  @click="toggleSound">
-        <span v-if="sound">Sound Off</span>
-        <span v-else>Sound On</span>
+     <div class="absolute bottom-12 left-12 cursor-pointer"  @click="toggleSound">
+        <span v-if="sound">
+          <FlFilledSpeakerOff class="h-10 w-10 text-red-500" />
+        </span>
+        <span v-else>
+          <FlFilledSpeaker2 class="h-10 w-10 text-red-500" />
+        </span>
     </div>
 </template>
 <script setup lang="ts">
 import { soundEnabled } from '../stores/settings';
+import { FlFilledSpeaker2, FlFilledSpeakerOff } from '@kalimahapps/vue-icons'
 
 const sound = soundEnabled;
 
